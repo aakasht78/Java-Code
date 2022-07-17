1af520b66793ae689dd9da1cc372628f48e23cb9
Aakash <aakashtiwari605@gmail.com>
2022-07-17 21:21:24 +0530

    added calculator.java

---
+++ Programs/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public static void main(String[] args) {
+        add();
+        sub();
+        div();
+    }
+    public static void add(){
+        int a =3 , b =4;
+        System.out.println(a+b);
+    }
+    public static void sub(){
+        int a =4 , b =2;
+        System.out.println(a-b);
+    }
+    public static void div(){
+        int a =7 , b =3;
+        System.out.println(a/b);
+    }
+}
