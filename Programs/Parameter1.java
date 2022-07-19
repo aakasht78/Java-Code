728665a6b46223340ded681d3523a763ff9473c3
Aakash <aakashtiwari605@gmail.com>
2022-07-19 13:33:02 +0530

    More Code

---
+++ Programs/Parameter1.java
@@ -0,0 +1,14 @@
+class Parameter1 {
+    public static void power(int base, int raise){
+        int pow = 1;
+        for (int i = 0; i < raise; i++) {
+            pow*=base;
+        }
+        System.out.println(pow);
+    }
+    public static void main(String[] args) {
+        power(3,4);
+        power(5+5,4);
+        power(2*6/3,3   );
+    }
+}
