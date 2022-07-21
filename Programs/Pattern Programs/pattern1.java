67a0cf03cad93c0ea741251ea455f0896bbc7353
Aakash <aakashtiwari605@gmail.com>
2022-07-21 15:33:09 +0530

    Pattern Programs added

---
+++ Programs/Pattern Programs/pattern1.java
@@ -0,0 +1,12 @@
+class pattern1 {
+    public static void main(String[] args) {
+        int count = 1;
+        for(int i=0; i<3;i++){
+            for(int j=0;j<=3;j++){
+                System.out.println(count + " Hi");
+                count++;
+
+            }
+        }
+    }    
+}
