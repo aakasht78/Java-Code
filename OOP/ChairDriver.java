33af9f9920905284d1fdd1220e458f95c394c2f5
Aakash <aakashtiwari605@gmail.com>
2022-08-08 21:09:19 +0530

    OOPs Started

---
+++ OOP/ChairDriver.java
@@ -0,0 +1,12 @@
+class ChairDriver
+{
+ public static void main(String [] args)
+ {
+	Chair c1 = new Chair();
+	Chair c2 = new Chair();
+	Chair c3 = new Chair();
+	System.out.println(c1);
+	System.out.println(c2);
+	System.out.println(c3);
+ }
+}
---
+++ OOP/LaptopDriver.java
@@ -0,0 +1,10 @@
+public class LaptopDriver {
+    public static void main(String[] args) {
+        Laptop L1 = new Laptop();
+        Laptop L12 = new Laptop();
+        Laptop L111 = new Laptop();
+        System.out.println(L1);
+        System.out.println(L12);
+        System.out.println(L111);
+    }
+}
