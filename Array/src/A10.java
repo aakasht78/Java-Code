acde026e807ce9287ed6bea259a4dd17c72d707b
Aakash <aakashtiwari605@gmail.com>
2022-08-23 20:13:38 +0530

    Array

---
+++ Array/src/A10.java
@@ -0,0 +1,10 @@
+class A10 {
+    public static void main(String[] args) {
+        int[] a = { 10 , 20 , 30 , 40 , 50, 60} ;
+        for (int i = 0; i <= a.length; i++) {
+          if (i%2!=0) {
+            System.out.println(a[i]+ " ");
+          }  
+        }
+    }    
+}
--- Array/src/A11.java
+++ Array/src/A11.java
@@ -1,10 +1,10 @@
-// Print even element of the array
-public class A9 {
+// Print odd element of the array
+public class A11 {
     public static void main(String[] args) {
         int [] a = {1, 2, 3, 4, 5};
         for(int i = 0; i<a.length;i++)
         {
-            if (a[i] %2 == 0 ) {
+            if (a[i] %2 != 0 ) {
                 System.out.println(a[i] + " ");
             }
         }
