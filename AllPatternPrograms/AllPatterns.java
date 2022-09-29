cd4e178f4577b2b2a1d6187c92196199383e358f
Aakash <aakashtiwari605@gmail.com>
2022-09-29 21:08:38 +0530

    more pattern code adde

--- AllPatternPrograms/AllPatterns.java
+++ AllPatternPrograms/AllPatterns.java
@@ -1,11 +1,10 @@
 public class AllPatterns {
     public static void main(String[] args) {
-
-        int n=4;
-        Pattern1(n);
+        int n=5;
+        Pattern4(n);
         // Pattern2(n);
-
 }
+// ######################### Pattern-1  #######################################
     public static void Pattern1(int n){
     for (int i = 1; i <n; i++) {
         for (int j = 0; j <=n; j++) {
@@ -22,7 +21,10 @@ public class AllPatterns {
  *  
  */
    } 
+// ######################### Pattern-1 Ends ######################################
 
+
+// ######################### Pattern-2  #######################################
 
    public static void Pattern2(int n){
     for(int i =1;i<=n;i++){
@@ -40,4 +42,51 @@ public class AllPatterns {
  *  
  */
    }
+   // ######################### Pattern-2 Ends  #################################
+
+
+// ######################### Pattern-3  #######################################
+
+   public static void Pattern3(int n){
+   for(int i=0;i<=4;i++){
+    for (int j = 0; j < n-i; j++) {
+        System.out.print("*" + " ");
+    }
+    System.out.println();
+   }
+  }
+
+/*
+    * * * *
+    * * * 
+    * * 
+    *   
+ */
+// ######################### Pattern-3 Ends  #################################
+
+// ######################### Pattern-4  ######################################
+
+
+
+  public static void Pattern4(int n){
+
+    for (int i = 1; i<=n; i++) {
+        for (int space=1; space<=n-i; space++) {
+            System.out.print(" ");
+        }
+            for (int j= 1 ; j<=2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+  
+/* 
+          *
+         ***
+        *****
+       *******
+*/
+// ######################### Pattern-4 Ends  #################################
+
 }
